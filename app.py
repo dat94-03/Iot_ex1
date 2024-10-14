5b49d9039e16cf90d1cf8d1a0b5b994310fced83
dat94-03 <tiendat942003@gmail.com>
2024-10-14 11:17:51 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Data storage (in-memory for simplicity)
+data_store = []
+
+# POST request to save data
+@app.route('/api/request-data', methods=['POST'])
+def handle_post_request():
+    request_data = request.json
+    device_id = request_data.get('deviceId')
+    qr_code_id = request_data.get('qrCodeId')
+    qr_code_value = request_data.get('qrCodeValue')
+
+    # Save data to the in-memory data store
+    new_data = {
+        'deviceId': device_id,
+        'qrCodeId': qr_code_id,
+        'qrCodeValue': qr_code_value
+    }
+    data_store.append(new_data)
+
+    return jsonify(new_data), 201  # Return 201 Created status code
+
+# GET request to retrieve all data
+@app.route('/api/request-data/all', methods=['GET'])
+def get_all_data():
+    if data_store:
+        return jsonify(data_store)
+    else:
+        return 'No data available', 404  # Return 404 if no data is present
+
+# GET request to retrieve data by deviceId
+@app.route('/api/request-data/<device_id>', methods=['GET'])
+def get_data_by_device_id(device_id):
+    # Search for data with the matching deviceId
+    matching_data = [item for item in data_store if item['deviceId'] == device_id]
+    
+    if matching_data:
+        return jsonify(matching_data)
+    else:
+        return 'No data found for the given deviceId', 404  # Return 404 if not found
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
